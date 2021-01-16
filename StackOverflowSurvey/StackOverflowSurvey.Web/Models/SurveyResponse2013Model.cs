a0c11198dfa23fd8ccd67cb6398c2780e7571530
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-16 17:16:15 +0200

    Add SurveyResponse2013Model - nicomplete - migration takes very long

---
+++ StackOverflowSurvey/StackOverflowSurvey.Web/Models/SurveyResponse2013Model.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StackOverflowSurvey.Web.Models
+{
+    public class SurveyResponse2013Model
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string Age { get; set; }
+        public string ProgrammingExperience { get; set; }
+        public string Industry { get; set; }
+        public string CompanySize { get; set; }
+        public string Occupation { get; set; }
+        public string TotalDevsYourCompany { get; set; }
+        public string TotalDevsYourTeam { get; set; }
+        public string RolesInteractSysAdmin { get; set; }
+        public string RolesInteractDesigner { get; set; }
+        public string RolesInteractProductManager { get; set; }
+        public string RolesInteractTestQA { get; set; }
+        public string RolesInteractTechSupport { get; set; }
+        public string RolesInteractSalesMarketing { get; set; }
+        public string RolesInteractCustomers { get; set; }
+        public string RolesInteractFinance { get; set; }
+        public string RolesInteractHR { get; set; }
+        public string YourCompanyMobileAppIPhone { get; set; }
+        public string YourCompanyMobileAppIPad { get; set; }
+        public string YourCompanyMobileAppAndroidPhone{ get; set; }
+        public string YourCompanyMobileAppAndroidTablet{ get; set; }
+        public string YourCompanyMobileAppBlackberry{ get; set; }
+        public string YourCompanyMobileAppOther{ get; set; }
+        public string YourCompanyMobileAppNoApp{ get; set; }
+        public string HowYourCompanyMonetizeAdvertising{ get; set; }
+        public string HowYourCompanyMonetizeDirectSalesConsumer{ get; set; }
+        public string HowYourCompanyMonetizeDirectSalesCompany{ get; set; }
+        public string HowYourCompanyMonetizeSaaSSubscription{ get; set; }
+        public string HowYourCompanyMonetizeAppSales{ get; set; }
+        public string HowYourCompanyMonetizeConsulting{ get; set; }
+        public string HowYourCompanyMonetizeGrantFundRaising{ get; set; }
+        public string HowYourCompanyMonetizeOther{ get; set; }
+        public string TimeSpentWeekNewFeatures{ get; set; }
+        public string TimeSpentWeekRefactoring{ get; set; }
+        public string TimeSpentWeekBugs{ get; set; }
+        public string TimeSpentWeekTechSupport{ get; set; }
+        public string TimeSpentWeekMeetings{ get; set; }
+        public string TimeSpentWeekLearningNewSkill{ get; set; }
+        public string TimeSpentWeekInternetBrowsing{ get; set; }
+        public string TimeSpentWeekCommuting{ get; set; }
+        public string PurchaseRoleRecommender { get; set; }
+        public string PurchaseRoleInfluencer { get; set; }
+        public string PurchaseRolePurchaser { get; set; }
+        public string PurchaseRoleApprover { get; set; }
+        public string PurchaseRoleCheckWriter { get; set; }
+        public string PurchaseRoleNoInvolvement { get; set; }
+        public string PurchaseRoleImASeller { get; set; }
+        public string PurchaseTypeHardware { get; set; }
+        public string PurchaseTypeServers { get; set; }
+        public string PurchaseTypeSoftware { get; set; }
+        public string PurchaseTypeUserEquipment { get; set; }
+        public string PurchaseTypeConsultants { get; set; }
+        public string PurchaseTypeOther { get; set; }
+        public string Budget { get; set; }
+        public string LanguageProC { get; set; }
+        public string LanguageProCPlusPlus { get; set; }
+        public string LanguageProCSharp { get; set; }
+        public string LanguageProJava { get; set; }
+        public string LanguageProJavaScript { get; set; }
+        public string LanguageProJQuery { get; set; }
+        public string LanguageProJQuery2 { get; set; }
+        public string LanguageProNodeJs { get; set; }
+        public string ObjectiveC { get; set; }
+        public string LanguageProPHP { get; set; }
+        public string LanguageProPython { get; set; }
+        public string LanguageProRuby { get; set; }
+        public string LanguageProSQL { get; set; }
+        public string LanguageProOther { get; set; }
+    }
+}
