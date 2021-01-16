9b0c7e30d6e4be8fe4a16920b33b7631899ba347
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-16 21:00:59 +0200

    Add SurveyResponse2016Model

---
+++ StackOverflowSurvey/StackOverflowSurvey.Web/Models/SurveyResponse2016Model.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StackOverflowSurvey.Web.Models
+{
+    public class SurveyResponse2016Model
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string Age { get; set; }
+        public string Gender { get; set; }
+        public string LanguageProC { get; set; }
+        public string LanguageProCPlusPlus { get; set; }
+        public string LanguageProCPlusPlus11 { get; set; }
+        public string LanguageProCSharp { get; set; }
+        public string LanguageProGo { get; set; }
+        public string LanguageProJava { get; set; }
+        public string LanguageProJavaScript { get; set; }
+        public string LanguageProNodeJs { get; set; }
+        public string LanguageProObjectiveC { get; set; }
+        public string LanguageProPerl { get; set; }
+        public string LanguageProPHP { get; set; }
+        public string LanguageProPython { get; set; }
+        public string LanguageProRuby { get; set; }
+        public string LanguageProRust { get; set; }
+        public string LanguageProSQL { get; set; }
+        public string LanguageProSQLServer { get; set; }
+        public string LanguageProSwift { get; set; }
+    }
+}
