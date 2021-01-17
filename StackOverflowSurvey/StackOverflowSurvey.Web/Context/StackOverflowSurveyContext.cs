0155c791b3d65fbb66bccd8b4fa251af0ad24af5
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-17 23:02:32 +0200

    Add 2019 table to db

--- StackOverflowSurvey/StackOverflowSurvey.Web/Context/StackOverflowSurveyContext.cs
+++ StackOverflowSurvey/StackOverflowSurvey.Web/Context/StackOverflowSurveyContext.cs
@@ -22,5 +22,6 @@ namespace StackOverflowSurvey.Web.Context
         public DbSet<SurveyResponse2016Model> SurveyResponses2016 { get; set; }
         public DbSet<SurveyResponse2017Model> SurveyResponses2017 { get; set; }
         public DbSet<SurveyResponse2018Model> SurveyResponses2018 { get; set; }
+        public DbSet<SurveyResponse2019Model> SurveyResponses2019 { get; set; }
     }
 }
---
+++ StackOverflowSurvey/StackOverflowSurvey.Web/Migrations/20210117210152_Add2019.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StackOverflowSurvey.Web.Migrations
+{
+    public partial class Add2019 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SurveyResponses2019",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Country = table.Column<string>(nullable: true),
+                    LanguageProC = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    LanguageProCPlusPlus = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    LanguageProCSharp = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    LanguageProGo = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    LanguageProJava = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    LanguageProJavaScript = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    LanguageProNodeJs = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    LanguageProObjectiveC = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    LanguageProPerl = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    LanguageProPHP = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    LanguageProPython = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    LanguageProRuby = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    LanguageProRust = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    LanguageProSQL = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    LanguageProSQLServer = table.Column<string>(type: "VARCHAR(100)", nullable: true),
+                    LanguageProSwift = table.Column<string>(type: "VARCHAR(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SurveyResponses2019", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SurveyResponses2019");
+        }
+    }
+}
