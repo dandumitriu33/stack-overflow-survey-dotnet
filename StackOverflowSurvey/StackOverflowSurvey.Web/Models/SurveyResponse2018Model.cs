a333b9b5b0f82201d988ea5038717f0aadf80075
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-17 21:25:40 +0200

    Add SurveyResponse2018Model

---
+++ StackOverflowSurvey/StackOverflowSurvey.Web/Models/SurveyResponse2018Model.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StackOverflowSurvey.Web.Models
+{
+    public class SurveyResponse2018Model
+    {
+        public string Country { get; set; }
+        [Column(TypeName = "VARCHAR(100)")]
+        public string LanguageProC { get; set; }
+        [Column(TypeName = "VARCHAR(100)")]
+        public string LanguageProCPlusPlus { get; set; }
+        [Column(TypeName = "VARCHAR(100)")]
+        public string LanguageProCSharp { get; set; }
+        [Column(TypeName = "VARCHAR(100)")]
+        public string LanguageProGo { get; set; }
+        [Column(TypeName = "VARCHAR(100)")]
+        public string LanguageProJava { get; set; }
+        [Column(TypeName = "VARCHAR(100)")]
+        public string LanguageProJavaScript { get; set; }
+        [Column(TypeName = "VARCHAR(100)")]
+        public string LanguageProNodeJs { get; set; }
+        [Column(TypeName = "VARCHAR(100)")]
+        public string LanguageProObjectiveC { get; set; }
+        [Column(TypeName = "VARCHAR(100)")]
+        public string LanguageProPerl { get; set; }
+        [Column(TypeName = "VARCHAR(100)")]
+        public string LanguageProPHP { get; set; }
+        [Column(TypeName = "VARCHAR(100)")]
+        public string LanguageProPython { get; set; }
+        [Column(TypeName = "VARCHAR(100)")]
+        public string LanguageProRuby { get; set; }
+        [Column(TypeName = "VARCHAR(100)")]
+        public string LanguageProRust { get; set; }
+        [Column(TypeName = "VARCHAR(100)")]
+        public string LanguageProSQL { get; set; }
+        [Column(TypeName = "VARCHAR(100)")]
+        public string LanguageProSQLServer { get; set; }
+        [Column(TypeName = "VARCHAR(100)")]
+        public string LanguageProSwift { get; set; }
+    }
+}
