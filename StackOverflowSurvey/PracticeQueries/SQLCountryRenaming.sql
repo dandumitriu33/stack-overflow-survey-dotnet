a073d6e7091e35a2eba2ad182de36bdf3eb4a397
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-20 11:01:05 +0200

    Refactor country names for Ireland, Vietnam, South Korea

--- StackOverflowSurvey/PracticeQueries/SQLCountryRenaming.sql
+++ StackOverflowSurvey/PracticeQueries/SQLCountryRenaming.sql
@@ -2,6 +2,18 @@
 --	SET Country = 'United States'
 --	WHERE Country = 'United States of America'
 
-UPDATE SurveyResponses2020
-	SET Country = 'Russia'
-	WHERE Country = 'Russian Federation'
+--UPDATE SurveyResponses2015
+--	SET Country = 'Russia'
+--	WHERE Country = 'Russian Federation'
+
+--UPDATE SurveyResponses2016
+--	SET Country = 'Ireland'
+--	WHERE Country = 'Ireland {Republic}'
+
+--UPDATE SurveyResponses2018
+--	SET Country = 'Vietnam'
+--	WHERE Country = 'Viet Nam'
+
+UPDATE SurveyResponses2015
+	SET Country = 'South Korea'
+	WHERE Country = 'Korea South'
