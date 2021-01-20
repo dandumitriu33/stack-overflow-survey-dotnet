15ac2a04ea9fa56c4542f2f64e07bf10d34ca22b
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-20 10:49:44 +0200

    Refactor Country names for US, Russia

---
+++ StackOverflowSurvey/PracticeQueries/SQLCountryRenaming.sql
@@ -0,0 +1,7 @@
+﻿--UPDATE SurveyResponses2013
+--	SET Country = 'United States'
+--	WHERE Country = 'United States of America'
+
+UPDATE SurveyResponses2020
+	SET Country = 'Russia'
+	WHERE Country = 'Russian Federation'
