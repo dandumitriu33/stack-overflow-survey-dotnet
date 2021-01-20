b1a7aee8061c5a98aad8b1e6b05f274cef0a955f
Dan Dumitriu <dandumitriu33@gmail.com>
2021-01-20 13:51:17 +0200

    Add all countries API endpoint

---
+++ StackOverflowSurvey/StackOverflowSurvey.Web/Controllers/QueryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackOverflowSurvey.Web.SQLDeliverySerivce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace StackOverflowSurvey.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QueryController : ControllerBase
+    {
+        private readonly ISQLDeliveryService _deliveryService;
+
+        public QueryController(ISQLDeliveryService deliveryService)
+        {
+            _deliveryService = deliveryService;
+        }
+
+        // GET: api/<QueryController>/all-countries
+        [Route("all-countries")]
+        public async Task<IActionResult> GetAllCountries()
+        {
+            var countries = await _deliveryService.GetAllCountries();
+            var payload = JsonSerializer.Serialize(countries);
+            return Ok(payload);
+        }
+    }
+}
